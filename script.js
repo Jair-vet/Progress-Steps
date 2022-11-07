af45aa25258b10572345159d72414a77eab59838
Jair <jairaceves56@gmail.com>
2022-11-07 17:50:44 -0600

    Progress-Steps

--- script.js
+++ script.js
@@ -1,15 +1,69 @@
-const panels = document.querySelectorAll('.panel')
+const progress = document.getElementById('progress')
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
+const circles = document.querySelectorAll('.circle')
+const info = document.querySelectorAll('.info-container')
 
-panels.forEach( panel => {
-    panel.addEventListener('click', () => {
-        removeActiveClasses()
-        panel.classList.add('active')
-    })
+let infoActive = 1
+let currentActive = 1
+
+
+next.addEventListener('click', () => {
+    currentActive++
+
+    if(currentActive > circles.length) {
+        currentActive = circles.length
+    }
+
+    showInfo()
+
+    update()
 })
 
-function removeActiveClasses() {
-    panels.forEach( panel => {
-        panel.classList.remove('active')
+prev.addEventListener('click', () => {
+    currentActive--
+
+    if(currentActive < 1) {
+        currentActive = 1
+    }
+
+    showInfo()
+
+    update()
+})
+
+function showInfo() {
+    info.forEach( (info, idx) => {
+        const id = idx + 1
+        
+        if( currentActive != id ){
+            info.hidden = true;
+        } else {
+            info.removeAttribute('hidden')
+        }
     })
 }
 
+
+function update() {
+    circles.forEach((circle, idx) => {
+        if(idx < currentActive) {
+            circle.classList.add('active')
+        } else {
+            circle.classList.remove('active')
+        }
+    })
+
+    const actives = document.querySelectorAll('.active')
+
+    progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%'
+
+    if(currentActive === 1) {
+        prev.disabled = true
+    } else if(currentActive === circles.length) {
+        next.disabled = true
+    } else {
+        prev.disabled = false
+        next.disabled = false
+    }
+}
